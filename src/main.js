d7dc3004853a8fadf4acce9a22a2f75c9bca950e
Vyan <88295018+OctaviantoVyan@users.noreply.github.com>
2023-09-03 20:40:19 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+
+createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')
---
+++ src/quasar-user-options.js
@@ -0,0 +1,18 @@
+
+import './styles/quasar.sass'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
+import '@quasar/extras/mdi-v4/mdi-v4.css'
+import '@quasar/extras/eva-icons/eva-icons.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  }
+}
